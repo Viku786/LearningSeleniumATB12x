2da134e0784a199610f6d0491f186d850f4a6a0c
Viku786 <kkvikash2016@gmail.com>
2025-08-25 12:33:55 +0530

    Initial Commit

--- src/test/java/com/thetestingacademy/Ex_01_Selenium_Basic/TestSelenium01.java
+++ src/test/java/com/thetestingacademy/Ex_01_Selenium_Basic/TestSelenium01.java
@@ -1,4 +1,24 @@
 package com.thetestingacademy.Ex_01_Selenium_Basic;
 
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class TestSelenium01 {
+
+    @Test
+    public void test_data(){
+        Assert.assertEquals("vikash","vikash");
+    }
+
+    @Test
+    public void test_OpenVWOLoginPage(){
+        // Open VWO.com, right?
+        FirefoxDriver driver = new FirefoxDriver();
+        driver.get("https://app.vwo.com");
+        driver.quit();
+
+
+    }
 }
